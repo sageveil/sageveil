e0f0d5bd8ace28b06cec877438cae33bedbbf224
Dawid Gąbka <gabka.daw@gmail.com>
2025-10-19 04:28:28 +0200

    chore: set release configuration to use conventional commits for version determination

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,14 @@
+import { default as nxScopes } from '@commitlint/config-nx-scopes';
+
 const config = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [...nxScopes.utils.getProjects(ctx), 'release'],
+    ],
+  },
 };
 
 export default config;
